fa7f0322a64ed40ed52940c4454715f70763d428
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-09-26 13:17:00 +1000

    feat: step-1 adding-two-numbers

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -11,20 +11,12 @@ Tests for `calc` module.
 import pytest
 
 
-from calc import calc
+from calc.calc import Calc
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def test_add_two_numbers():
+    c = Calc()
 
+    res = c.add(4, 5)
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument.
-    """
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    assert res == 9
